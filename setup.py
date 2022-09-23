2cbb2ac2eba0cb4ebda3909be4df125749d39456
MrCasndra <tazgwel@hotmail.fr>
2022-09-23 12:45:56 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='scrapgfx',
+    version='0.1',
+    packages=['sitesettings'],
+    package_dir={'': 'tests'},
+    url='https://github.com/soltal/scrapgfx',
+    license='MIT',
+    author='MrCasndra',
+    author_email='',
+    description='price comparison'
+)
